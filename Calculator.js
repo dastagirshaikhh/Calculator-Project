8953dfd9fc56eaabb4bd6262be6972047d25d8e4
Mohommed Dastagir Shaikh <111683833+dastagirshaikhh@users.noreply.github.com>
2024-02-24 14:48:03 +0530

    static/

---
+++ Calculator.js
@@ -0,0 +1,26 @@
+// function Result() {
+//     var num = document.getElementById('res').value;
+//     var num2 = eval(num);
+//     document.getElementById('res').value = num2;
+// }
+
+
+function Result() {
+    const inputString = document.getElementById('res').value;
+    const requestData = { js_string: inputString };
+
+    fetch('/result', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const inputElement = document.getElementById('res');
+        inputElement.value = data.python_string;
+        console.log(data.python_string)
+    })
+    .catch(error => console.error('Error:', error));
+}
